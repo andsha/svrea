ce61e17d588e47eda81cfd06340d82e0874641b9
andsha <andreyshavorskiy@andreys-MacBook-Pro.local>
2017-01-31 21:33:42 +0100

    .

--- svrea_script/models.py
+++ svrea_script/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Info(models.Model):
-    date_started = models.DateTimeField(auto_now=True, primary_key=True)
-    script = models.CharField(max_length = 200)
+    started = models.DateTimeField(auto_now=True, primary_key=True)
+    user = models.CharField(max_length=50)
+    config = models.CharField(max_length = 200)
     status = models.CharField(max_length=200)
     comment = models.CharField(max_length=500)
 
