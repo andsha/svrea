061c50b18505da2858727feddf275af4f9966be9
andsha <andreyshavorskiy@andreys-MacBook-Pro.local>
2017-02-01 19:03:20 +0100

    .

--- svrea_script/views.py
+++ svrea_script/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import logout
 
+from script.svrea_script import svrea_script
+
 
 @login_required(redirect_field_name = "", login_url="/")
 @permission_required('svrea_script.can_run_script')
 def script_run(request):
 
+    text = 'nothing'
     if request.POST.get('submit') == 'Log Out':
         logout(request)
         return redirect("index")
 
+    if request.POST.get('run_script') == '':
+        text = request.POST.get('script_params')
+        script = svrea_script()
+        script.run(text)
+
+
 
 
     context = {
-        "text" : "You can run scrip"
+        "text" : text
     }
     return render(request, "svrea_script/run.html", context=context)
 
