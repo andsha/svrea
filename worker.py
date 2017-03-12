c6f5d4cd1f6091cab820e1df90c3f8a86186fa5c
andsha <scr06as@gmail.com>
2017-03-12 16:50:49 +0100

    .

--- worker.py
+++ worker.py
@@ -7,10 +7,10 @@ from manage import DEFAULT_SETTINGS_MODULE
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
 
 #*****************************  USE WHEN RUN LOCAL  ******************************
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-f = open(BASE_DIR + '/svrea/.env', 'r')
-DATABASE_URL = f.readline().split('=')[1].strip().strip("'")
-os.environ.setdefault("DATABASE_URL", DATABASE_URL)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# f = open(BASE_DIR + '/svrea/.env', 'r')
+# DATABASE_URL = f.readline().split('=')[1].strip().strip("'")
+# os.environ.setdefault("DATABASE_URL", DATABASE_URL)
 
 #**********************************************************************************
 
