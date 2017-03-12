aaf2571ef90140b29471be900314356356fc669c
andsha <scr06as@gmail.com>
2017-03-12 11:31:06 +0100

    .

--- worker.py
+++ worker.py
@@ -6,12 +6,12 @@ from rq import Worker, Queue, Connection
 
 #*****************************  USE WHEN RUN LOCAL  ******************************
 
-# from manage import DEFAULT_SETTINGS_MODULE
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# f = open(BASE_DIR + '/svrea/.env', 'r')
-# DATABASE_URL = f.readline().split('=')[1].strip().strip("'")
-# os.environ.setdefault("DATABASE_URL", DATABASE_URL)
+from manage import DEFAULT_SETTINGS_MODULE
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+f = open(BASE_DIR + '/svrea/.env', 'r')
+DATABASE_URL = f.readline().split('=')[1].strip().strip("'")
+os.environ.setdefault("DATABASE_URL", DATABASE_URL)
 
 #**********************************************************************************
 
@@ -22,8 +22,8 @@ redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    # import django
-    # django.setup()
+    import django
+    django.setup()
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
