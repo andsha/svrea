99279a0b2ca06dc5c2c8e066470a12285881e388
andsha <scr06as@gmail.com>
2017-08-10 19:10:37 +0200

    .

--- svrea_etl/migrations/0018_remove_etllistingsdaily_sold_daysbeforesold_avg.py
+++ svrea_etl/migrations/0018_remove_etllistingsdaily_sold_daysbeforesold_avg.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('svrea_etl', '0017_auto_20170810_1649'),
+        ('svrea_etl', '0016_etllistingsdaily_sold_daysbeforesold_avg'),
     ]
 
     operations = [
