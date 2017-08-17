9fe3bc612bbc31153043c77d85299348d1ce12af
andsha <scr06as@gmail.com>
2017-08-17 22:27:47 +0200

    .

--- script/svrea_script.py
+++ script/svrea_script.py
@@ -517,8 +517,8 @@ class Svrea_script():
             'fill_listings_daily_stats',
             'finish'
         ]
-        startofperiod = datetime.datetime.strptime(self.options['etlRange'].split(':')[0], "%Y-%m-%d")
-        endofperiod = datetime.datetime.strptime(self.options['etlRange'].split(':')[1], "%Y-%m-%d")
+        startofperiod = datetime.datetime.strptime(self.options['etlRange'].split(':')[0], "%Y-%m-%d").date()
+        endofperiod = datetime.datetime.strptime(self.options['etlRange'].split(':')[1], "%Y-%m-%d").date()
 
         if self.options['etlPeriodType'] == 'Weekly':
             # find monday of this week
@@ -618,7 +618,7 @@ class Svrea_script():
 
         for gtype in geographic_types:
             listing = Listings.objects.values('address__county' if gtype == 'county' else 'address__municipality' if gtype == 'municipality' else 'address__country')\
-                .filter(Q(datepublished__date__lte=dayTo) & (Q(dateinactive__isnull=True) | Q(dateinactive__date__gt=dayFrom))) \
+                .filter(Q(datepublished__date__lt=dayTo) & (Q(dateinactive__isnull=True) | Q(dateinactive__date__gte=dayFrom))) \
                 .annotate(listing_counts=Count('booliid'),
                           listing_price_avg = Avg('latestprice'),
                           listing_price_med = Percentile(expression='latestprice', percentiles=0.5),
@@ -639,7 +639,7 @@ class Svrea_script():
                           )
 
             sold = Listings.objects.values('address__county' if gtype == 'county' else 'address__municipality' if gtype == 'municipality' else 'address__country') \
-                .filter(Q(datesold__date__lte = dayTo) & Q(datesold__date__gt = dayFrom)) \
+                .filter(Q(datesold__date__lt = dayTo) & Q(datesold__date__gte = dayFrom)) \
                 .annotate(sold_year = Extract_date('datesold', dtype = 'year'))\
                 .annotate(sold_counts=Count('booliid'),
                           sold_price_avg=Avg('latestprice'),
